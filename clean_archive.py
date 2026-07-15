cc41562346160ee37587689394108d782da738e3
Rey Squillace <Reynier Squillace reynier@virginia.edu>
2026-07-15 10:56:03 -0400

    back to argparse!

--- clean_archive.py
+++ clean_archive.py
@@ -18,24 +18,25 @@ from coast_guard import config
 import argparse
 import psrchive as ps
 import os
-import click
 
 def apply_rcvrstd_cleaner(ar):
     """This function applies the cleaner specified by a config file for the receiver."""
-    
+
     print("Applying the receiver cleaner")
     rcvrstd_cleaner = cleaners.load_cleaner('rcvrstd')
     rcvrstd_cleaner.run(ar)
 
 def apply_surgical_cleaner(ar, tmp, cthresh=None, sthresh=None, plot=False, aggressive=False, iterations=1):
-    
+
     """Apply the surgical cleaner to an archive in place.
 
         Inputs:
             ar: The psrchive Archive object to clean.
             tmp: Path to the (optionally 2D) template file to use.
-            cthresh: Channel threshold in sigma. (Default: 7.0)
-            sthresh: Sub-int threshold in sigma. (Default: 7.0)
+            cthresh: Channel threshold in sigma. (Default: None, i.e. deferred
+                to -C/--config if given, else 7.0/5.0 per --aggressive)
+            sthresh: Sub-int threshold in sigma. (Default: None, i.e. deferred
+                to -C/--config if given, else 7.0/5.0 per --aggressive)
             plot: If True, produce diagnostic plots. (Default: False)
             aggressive: If True, use more aggressive cleaning thresholds
                 and algorithms. (Default: False)
@@ -49,7 +50,7 @@ def apply_surgical_cleaner(ar, tmp, cthresh=None, sthresh=None, plot=False, aggr
     print("\t channel threshold = {0}".format(cthresh))
     print("\t  subint threshold = {0}".format(sthresh))
     print("\t  iterations = {0}".format(iterations))
-    
+
     surgical_cleaner = cleaners.load_cleaner('surgical')
     param_parts = [
         "template={0}".format(tmp),
@@ -96,45 +97,55 @@ def apply_bandwagon_cleaner(ar, badchantol=None, badsubtol=None):
         bandwagon_cleaner.parse_config_string(",".join(param_parts))
     bandwagon_cleaner.run(ar)
 
-#switching to click for more versatility
-@click.command(help="Run MeerGuard on input archive file")
-#click will automatically send an error message if the user doesn't input -a and -T!
-@click.argument("archive_paths", nargs=-1, required=True)
-@click.option("-T", "--template", "template_path", type=str, required=True,
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run MeerGuard on input archive file")
+    parser.add_argument("archive_paths", type=str, nargs='+')
+    parser.add_argument("-T", "--template", type=str, dest="template_path", required=True,
               help="REQUIRED: Path to the 2D template file")
-@click.option("-c", "--chanthresh", "chan_thresh", type=float, default=None,
+    parser.add_argument("-c", "--chanthresh", type=float, dest="chan_thresh", default=None,
               help="Channel threshold (in sigma) [default = 7.0 (5.0 with --aggressive)]")
-@click.option("-s", "--subthresh", "subint_thresh", type=float, default=None,
+    parser.add_argument("-s", "--subthresh", type=float, dest="subint_thresh", default=None,
               help="Subint threshold (in sigma) [default = 7.0 (5.0 with --aggressive)]")
-@click.option("-bc", "--badchantol", "badchantol", type=float, default=None,
+    parser.add_argument("-bc", "--badchantol", type=float, dest="badchantol", default=None,
               help="Fraction of bad channels threshold [default = 0.95 (0.8 with --aggressive)]")
-@click.option("-bs", "--badsubtol", "badsubtol", type=float, default=None,
+    parser.add_argument("-bs", "--badsubtol", type=float, dest="badsubtol", default=None,
               help="Fraction of bad subints threshold [default = 0.95 (0.8 with --aggressive)]")
-@click.option("-o", "--outname", "output_name", type=str, default=None,
+    parser.add_argument("-o", "--outname", type=str, dest="output_name", default=None,
               help="Output archive name")
-@click.option("-e", "--extension", "extension", type=str, default=None,
+    parser.add_argument("-e", "--extension", type=str, dest="extension", default=None,
               help="Output file extension")
-@click.option("-plot", "--plot", "plot", is_flag=True, default=False)
-@click.option("-O", "--outpath", "output_path", type=str, default=os.getcwd(),
+    parser.add_argument("-plot", "--plot", dest='plot', action='store_true', default=False)
+    parser.add_argument("-O", "--outpath", type=str, dest="output_path", default=os.getcwd(),
               help="Output path [default = CWD]")
-@click.option("-ag", "--aggressive", "aggressive", is_flag=True, default=False,
+    parser.add_argument("-ag", "--aggressive", dest='aggressive', action='store_true', default=False,
               help="Whether to use more aggressive cleaning thresholds and algorithms")
-@click.option("-i", "--iterations", "iterations", type=int, default=1,
+    parser.add_argument("-i", "--iterations", type=int, dest="iterations", default=1,
               help="Number of iterations to run the surgical cleaner [default = 1]")
-@click.option("-C", "--config", "config_path", type=str, default=None,
+    parser.add_argument("-C", "--config", type=str, dest="config_path", default=None,
               help="Custom config file for misbehaving receivers. Inputting UHF or L will "
               "automatically load the MeerKAT config files for those receivers. Inputting "
               "UWL will automatically load the config file for the Murriyang ultrawide "
               "low frequency receiver.")
 
-def main(archive_paths, template_path, chan_thresh, subint_thresh, badchantol,
-         badsubtol, output_name, extension, plot, output_path, aggressive, iterations,
-         config_path):
-    
+    args = parser.parse_args()
+    archive_paths = args.archive_paths
+    template_path = args.template_path
+    chan_thresh = args.chan_thresh
+    subint_thresh = args.subint_thresh
+    badchantol = args.badchantol
+    badsubtol = args.badsubtol
+    output_name = args.output_name
+    extension = args.extension
+    plot = args.plot
+    output_path = args.output_path
+    aggressive = args.aggressive
+    iterations = args.iterations
+    config_path = args.config_path
+
     #raise error if user tries to write multiple input files to one output name
     #have to use extension for that option
     if output_name is not None and len(archive_paths) > 1:
-        raise click.UsageError(
+        parser.error(
         "-o cannot be used with multiple files. Your options are:"
         "1. Use the -e option to replace extensions on each file."
         "2. Use default output file names."
@@ -143,7 +154,7 @@ def main(archive_paths, template_path, chan_thresh, subint_thresh, badchantol,
 
     #make sure there's either -o or -e, not both
     if output_name is not None and extension is not None:
-        raise click.UsageError(
+        parser.error(
             "-o and -e cannot be used together. "
             "Please make your mind up... :)"
         )
@@ -151,15 +162,15 @@ def main(archive_paths, template_path, chan_thresh, subint_thresh, badchantol,
     # threshold the user did NOT set explicitly on the command line falls
     # back to the documented aggressive value; otherwise it falls back to the
     # normal default. Explicit user overrides are preserved either way.
-    
+
     if aggressive:
         threshold_defaults = {'chan_thresh': 5.0, 'subint_thresh': 5.0,
                               'badchantol': 0.8, 'badsubtol': 0.8}
     else:
         threshold_defaults = {'chan_thresh': 7.0, 'subint_thresh': 7.0,
                               'badchantol': 0.95, 'badsubtol': 0.95}
-    
-        
+
+
     defer_to_config = (config_path is not None) and not aggressive
 
     if chan_thresh is None and not defer_to_config:
@@ -171,7 +182,7 @@ def main(archive_paths, template_path, chan_thresh, subint_thresh, badchantol,
     if badsubtol is None and not defer_to_config:
         badsubtol = threshold_defaults['badsubtol']
 
-    #grab custom config values if sent in with -C 
+    #grab custom config values if sent in with -C
     if config_path is not None:
 
         if config_path in ('UHF', 'uhf'):
@@ -212,8 +223,6 @@ def main(archive_paths, template_path, chan_thresh, subint_thresh, badchantol,
         apply_bandwagon_cleaner(loaded_archive, badchantol=badchantol, badsubtol=badsubtol)
 
         # Unload the Archive file
+        out_name = os.path.join(output_path, out_name)
         print("Unloading the cleaned archive: {0}".format(out_name))
         loaded_archive.unload(str(out_name))  # need to typecast to str here because otherwise Python converts to a unicode string which the PSRCHIVE library can't parse
-
-if __name__ == "__main__":
-    main()
